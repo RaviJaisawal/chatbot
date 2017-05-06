4bc8ab0f46ebe5a11263e135a791f81b7d890a63
Ravi Jaisawal <j.ravi786@gmail.com>
2017-05-06 22:36:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function sendText(sender,text){
     let messageData = { text : text};
 
     request({
-        url:"https://graph.facebook.com/v2.6/memessages/",
+        url:"https://graph.facebook.com/v2.6/me/messages/",
         qs  : {access_token:token},
         method :"POST",
         json :{
